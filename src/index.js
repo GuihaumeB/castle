2f1e328326cea4c5323f3bee29fb24863824adf3
GuihaumeB <guihaume.buevoz@devinci.fr>
2019-02-07 13:43:17 +0100

    v1.3

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
-// Extract all submissions from the loaded page
-function pageFunction(context) {
-    let $ = context.jQuery;
-    let submissions = $('.athing').toArray();
+const castle = require('./scrape.js');
+const michelin = require('./michelinScraping.js');
+let fs = require('fs');
 
-    return submissions.map(function (el) {
-        let $el = $(el), $next = $el.next();
+'use strict';
+console.log("debut");
 
-        return {
-            Rank: $el.find('.rank').text(),
-            Title: $el.find('.storylink').text(),
-            Link: $el.find('.storylink').attr('href'),
-            Score: parseInt($next.find('.score').text()),
-            Author: $next.find('.hnuser').text(),
-            Time: $next.find('.age').text()
-        };
-    });
+const hotelJSON = castle.getHotelsJSON();
+const JSONMichelin = michelin.getRestaurantsJSON();
+
+fs.writeFileSync("RelaisEtoiles.json",JSON.stringify(findMutualChefsAndPCs(hotelJSON, JSONMichelin)));
+
+function findMutualChefsAndPCs(ListeHotels, ListeMichelin) {
+    let HotelsEtoiles = [];
+    for (let i = 0; i < ListeHotels.length; i++) {
+        for (let j = 0; j < ListeMichelin.length; j++) {
+            if (ListeHotels[i].chef === ListeMichelin[j].chef && ListeHotels[i].postalCode === ListeMichelin[j].postalCode) {
+                HotelsEtoiles.push({"hotelName": ListeHotels[i].name, "restaurantName": ListeMichelin[j].name, "postalCode": ListeHotels[i].postalCode, "chef": ListeHotels[i].chef, "url": ListeHotels[i].url, "price": ListeHotels[i].price })
+            }
+        }
+    }
+    return HotelsEtoiles;
 }
+
+console.log("fin");
